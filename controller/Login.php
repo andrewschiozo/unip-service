e39cc84086d026d615a0f3f09813143f634d4d48
Andrews Chiozo <andrews.chiozo@cggn.com.br>
2023-10-04 13:18:58 -0300

    feat: Controller Login

---
+++ controller/Login.php
@@ -0,0 +1,22 @@
+<?php
+namespace controller;
+
+use util\Request;
+use util\Response;
+use util\Ldap;
+use util\JWT;
+use util\Config;
+
+class Login extends Controller
+{
+	public function __construct()
+	{
+        parent::__construct();
+	}
+
+	public function post()
+	{
+       
+	}
+
+}
